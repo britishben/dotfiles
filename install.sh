f3a5515b6c600c92c18acb402a91a60970e49a5f
Benjamin P. McKenna <britishben@gmail.com>
2017-01-20 11:31:36 +0000

    Handled the bash_completion/tma case for tmux attach completion

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#basic install script - pass the file to install as param, and it gets installed in ~.
+#now handles special case of tma completion.
 
 file=$1
 currentdir=`pwd`
 
-ln -sfv "$currentdir/$file" ~/$file
+if [ $1 != "tma" ]; then
+    ln -sfv "$currentdir/$file" ~/$file
+else
+    ln -sfv "/etc/bash_completion.d/tma"
+fi
