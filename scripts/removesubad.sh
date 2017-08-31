0ffdfe5e34496a60e65f19d970d0834f62c2e3c7
Benjamin P. McKenna <britishben@gmail.com>
2017-08-31 12:16:06 +0100

    Updated subtitle script a bit

--- scripts/removesubad.sh
+++ scripts/removesubad.sh
@@ -1,50 +1,51 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
+#Removes ads from downloaded subtitles
 
 #convert to proper format first
 
-chmod 777 *.srt
-dos2unix *.srt
+chmod 777 "*.srt"
+dos2unix "*.srt"
 
 #remove subtitle ads
 
 #addic7ed
-sed -i '/.*www\.addic7ed\.com.*/d' *.srt
+sed -i '/.*www\.addic7ed\.com.*/d' "*.srt"
 
 ### ENGLISH ###
 #vegusto
-sed -i '/Delicious Vegan Sausages, Cheese/d' *.en.srt && sed -i '/and Burgers - Vegusto.co.uk/d' *.en.srt
+sed -i '/Delicious Vegan Sausages, Cheese/d' "*.en.srt" && sed -i '/and Burgers - Vegusto.co.uk/d' "*.en.srt"
 #looklive
-sed -i '/Shop this shows fashion, download the/d' *.en.srt && sed -i '/"LookLive" app in iTunes/d' *.en.srt
+sed -i '/Shop this shows fashion, download the/d' "*.en.srt" && sed -i '/"LookLive" app in iTunes/d' "*.en.srt"
 #opensubtitles
-sed -i '/Support us and become VIP member /d' *.en.srt && sed -i '/to remove all ads from www.OpenSubtitles.org/d' *.en.srt
+sed -i '/Support us and become VIP member /d' "*.en.srt" && sed -i '/to remove all ads from www.OpenSubtitles.org/d' "*.en.srt"
 #gts-translation
-sed -i '/Professional Translation Services/d' *.en.srt && sed -i '/www.gts-translation.com/d' *.en.srt
+sed -i '/Professional Translation Services/d' "*.en.srt" && sed -i '/www.gts-translation.com/d' "*.en.srt"
 ###
 
 ### DUTCH ###
 #panther pro
-sed -i '/De Panther pro beste streambox van 2016 betaal geen abonnement meer kijk alles gratis/d' *.nl.srt && sed -i '/www.pantherpro.nl/d' *.nl.srt
+sed -i '/De Panther pro beste streambox van 2016 betaal geen abonnement meer kijk alles gratis/d' ""*.nl.srt"" && sed -i '/www.pantherpro.nl/d' "*.nl.srt"
 #tvbox
-sed -i '/De meest gebruiksvriendelijke streambox:/d' *.nl.srt && sed -i '/www.TvBoxPro.nl/d' *.nl.srt
+sed -i '/De meest gebruiksvriendelijke streambox:/d' "*.nl.srt" && sed -i '/www.TvBoxPro.nl/d' "*.nl.srt"
 #looklive
-sed -i '/Shop deze kleding op LookLive.com/d' *.nl.srt
+sed -i '/Shop deze kleding op LookLive.com/d' "*.nl.srt"
 #gratisstreamen
-sed -i '/Bekijk de reviews en bestel op GRATISSTREAMEN.NL/d' *.nl.srt
+sed -i '/Bekijk de reviews en bestel op GRATISSTREAMEN.NL/d' "*.nl.srt"
 ###
 
 ### FRENCH ###
 #looklive
-sed -i '/Achète la mode de cet ep via Loolive.com/d' *.fr.srt
-sed -i '/Ach�te la mode de cet ep via Loolive.com/d' *.fr.srt
+sed -i '/Achète la mode de cet ep via Loolive.com/d' "*.fr.srt"
+sed -i '/Ach�te la mode de cet ep via Loolive.com/d' "*.fr.srt"
 ###
 
 ### TBA ###
 
 #sync and corrected
-#sed -i '/.*Synced & corrected.*/d' *.srt
-#sed -i 'Sync et corrigé par.*' *.srt
-#sed -i 'Syncro par.*' *.srt
-#sed -i '/Resync by <font color=.*>/d'
+sed -i '/.*Synced & corrected.*/d' "*.srt"
+sed -i 'Sync et corrigé par.*' "*.srt"
+sed -i 'Syncro par.*' "*.srt"
+sed -i '/Resync by <font color=.*>/d' "*.srt"
 
 #tvsubtitles.net
-#sed -i '/<font color=.*>www.tvsubtitles.*$/d'
+sed -i '/<font color=.*>www.tvsubtitles.*$/d' "*.srt"
