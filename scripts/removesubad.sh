cab69259910f0ef767f812e1ff42ee184f80dc01
Benjamin P. McKenna <britishben@gmail.com>
2018-01-15 11:41:26 +0000

    Make the seds do two things

--- scripts/removesubad.sh
+++ scripts/removesubad.sh
@@ -13,20 +13,20 @@ sed -i '/.*www\.addic7ed\.com.*/d' "*.srt"
 
 ### ENGLISH ###
 #vegusto
-sed -i '/Delicious Vegan Sausages, Cheese/d' "*.en.srt" && sed -i '/and Burgers - Vegusto.co.uk/d' "*.en.srt"
+sed -i '/Delicious Vegan Sausages, Cheese/d; /and Burgers - Vegusto.co.uk/d' "*.en.srt"
 #looklive
-sed -i '/Shop this shows fashion, download the/d' "*.en.srt" && sed -i '/"LookLive" app in iTunes/d' "*.en.srt"
+sed -i '/Shop this shows fashion, download the/d; /"LookLive" app in iTunes/d' "*.en.srt"
 #opensubtitles
-sed -i '/Support us and become VIP member /d' "*.en.srt" && sed -i '/to remove all ads from www.OpenSubtitles.org/d' "*.en.srt"
+sed -i '/Support us and become VIP member /d; /to remove all ads from www.OpenSubtitles.org/d' "*.en.srt"
 #gts-translation
-sed -i '/Professional Translation Services/d' "*.en.srt" && sed -i '/www.gts-translation.com/d' "*.en.srt"
+sed -i '/Professional Translation Services/d; /www.gts-translation.com/d' "*.en.srt"
 ###
 
 ### DUTCH ###
 #panther pro
-sed -i '/De Panther pro beste streambox van 2016 betaal geen abonnement meer kijk alles gratis/d' ""*.nl.srt"" && sed -i '/www.pantherpro.nl/d' "*.nl.srt"
+sed -i '/De Panther pro beste streambox van 2016 betaal geen abonnement meer kijk alles gratis/d; /www.pantherpro.nl/d' "*.nl.srt"
 #tvbox
-sed -i '/De meest gebruiksvriendelijke streambox:/d' "*.nl.srt" && sed -i '/www.TvBoxPro.nl/d' "*.nl.srt"
+sed -i '/De meest gebruiksvriendelijke streambox:/d; /www.TvBoxPro.nl/d' "*.nl.srt"
 #looklive
 sed -i '/Shop deze kleding op LookLive.com/d' "*.nl.srt"
 #gratisstreamen
